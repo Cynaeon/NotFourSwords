3a16886c8ed5e8e80175685db16426b7092e5120
Joni Tasala <jonitasala@gmail.com>
2017-01-27 21:17:31 +0200

    Initial. Test scene with player movement, camera movement, lock on looks at enemy.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	[SerializeField] private float turnSpeed = 4.0f;
+	[SerializeField] private float height = 8.0f;
+	[SerializeField] private float distance = 7.0f;
+
+	public Transform player;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = new Vector3(player.position.x, player.position.y + height, player.position.z + distance);
+
+	}
+
+
+
+	void FixedUpdate()
+	{
+		offset = Quaternion.AngleAxis (Input.GetAxis ("HorizontalRightStick") * turnSpeed, Vector3.up) * offset;
+		//offset = Quaternion.AngleAxis (Input.GetAxis("VerticalRightStick") * turnSpeed, Vector3.right) * offset;
+
+		transform.position = player.position + offset; 
+		transform.LookAt (player.position);
+
+	}
+		
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed = 10.0F;
+	public Transform playerCamera;
+
+	private bool lockOn;
+
+	void Update() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		if (movement != Vector3.zero) {
+			movement = playerCamera.transform.TransformDirection (movement);
+			movement.y = 0.0f;
+
+			Quaternion rotation = new Quaternion (0, 0, playerCamera.rotation.z, 0);
+			transform.rotation = rotation;
+			transform.rotation = Quaternion.LookRotation (movement);
+
+		}
+		transform.Translate (movement * speed * Time.deltaTime, Space.World);
+
+		lockOn = Input.GetButton ("LockOn");
+		if (lockOn) {
+			GameObject enemy = GameObject.FindGameObjectWithTag ("Enemy");
+			transform.LookAt (enemy.transform);
+		}
+	}
+}
