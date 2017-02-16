8aa8902fee0e1562cf7370391fe625ac5b2691a4
Joni Tasala <jonitasala@gmail.com>
2017-02-16 17:28:49 +0200

    Invisible objects now emit cool particles to highlight them.

---
+++ Assets/Scripts/InvisibleParticles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleParticles : MonoBehaviour {
+
+    public Transform invisibleBlock;
+    
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var sh = ps.shape;
+        sh.box = new Vector3(invisibleBlock.transform.localScale.x + 0.5f, invisibleBlock.transform.localScale.z + 0.5f, 1);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
