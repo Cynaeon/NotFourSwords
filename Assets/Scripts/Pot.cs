7faf502a730af330d93a956a4099231873a442e7
Antti <akj.tuomisto@hotmail.com>
2017-03-06 13:44:34 +0200

    pot goes broke = pot goes bye bye. Prefab'd.

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -6,11 +6,33 @@ public class Pot : MonoBehaviour {
     
     public GameObject heart;
     private Animator anime;
+    private bool broken;
+    private float dropSpeed;
 
     void Start()
     {
         anime = GetComponentInChildren<Animator>();
+        dropSpeed = 0.2f; 
     }
+
+    void Update()
+    {
+        if (broken)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * dropSpeed);
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerProjectile")
+        {
+            Destroy();
+            broken = true;
+        }
+    }
+
 
     public void Destroy()
     {
@@ -19,14 +41,12 @@ public class Pot : MonoBehaviour {
         Instantiate(heart, pos, transform.rotation);
         Instantiate(heart, pos, transform.rotation);
         anime.Play("Break");
-        
+        Invoke("reallyDestroy", 4);
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void reallyDestroy()
     {
-        if (other.tag == "PlayerProjectile")
-        {
-            Destroy();
-        }
+        Destroy(gameObject);
     }
+
 }
