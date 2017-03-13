3779152ebfd0f6842abe37e9e91ac7992a528e4c
Joni Tasala <jonitasala@gmail.com>
2017-03-13 19:26:15 +0200

    New puzzle, "RotatingCube".

---
+++ Assets/Scripts/Puzzles/RotatingCube.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCube : MonoBehaviour {
+
+    public GameObject target;
+    public ParticleSystem destroyEffect;
+    public float rotateSpeed;
+
+    private int timesRotated;
+    private float Yrotation;
+    private float Zrotation;
+
+	// Use this for initialization
+	void Start () {
+        timesRotated = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target.GetComponent<Target>().activated)
+        {
+            Activate();
+        }
+        
+	}
+
+    public void Activate()
+    {
+        if (timesRotated == 0)
+        {
+            Zrotation += rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, Zrotation);
+            if (Zrotation >= 180)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+                target.GetComponent<Target>().Deactivate();
+                timesRotated++;
+            } 
+        }
+        else if (timesRotated == 1)
+        {
+            Yrotation += rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, Yrotation, Zrotation);
+            if (Yrotation >= 270)
+            {
+                transform.rotation = Quaternion.Euler(0, 270, 180);
+                target.GetComponent<Target>().Deactivate();
+                timesRotated++;
+            }
+        }
+        else if (timesRotated == 2)
+        {
+            Zrotation += rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, Yrotation, Zrotation);
+            if (Zrotation >= 360)
+            {
+                transform.rotation = Quaternion.Euler(0, 270, 360);
+                target.GetComponent<Target>().Deactivate();
+                timesRotated++;
+            }
+        }
+        else if (timesRotated == 3)
+        {
+            rotateSpeed += 1;
+            Zrotation += rotateSpeed * Time.deltaTime;
+            Yrotation += rotateSpeed * Time.deltaTime;
+            transform.localScale -= Vector3.one * Time.deltaTime * 2;
+            transform.rotation = Quaternion.Euler(0, Yrotation, Zrotation);
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+
+            if (transform.localScale.x < 0.1f)
+            {
+                Instantiate(destroyEffect, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/Target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    [HideInInspector] public bool activated;
+    private Color activeColor;
+    private Color deactiveColor;
+    private Renderer _rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+        activeColor = new Color(1, 0.5f, 0.5f, 1);
+        deactiveColor = _rend.material.color;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerProjectile")
+        {
+            Activate(); 
+        }
+    }
+
+    private void Activate()
+    {
+        activated = true;
+        _rend.material.color = activeColor;
+    }
+
+    public void Deactivate()
+    {
+        activated = false;
+        _rend.material.color = deactiveColor;
+    }
+}
