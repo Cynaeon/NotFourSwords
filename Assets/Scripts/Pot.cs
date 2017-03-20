31990727a8032c9f489951d0fa15d8c7d95d2ea0
The13Destroyer <the13destroyer@cock.li>
2017-03-20 17:40:30 +0200

    Changed first powerup, altered various values

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -29,7 +29,6 @@ public class Pot : MonoBehaviour {
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
         Quaternion rot = Quaternion.Euler(-90, 0, 0);
         Instantiate(heart, pos, rot);
-        Instantiate(heart, pos, rot);
         Instantiate(breakAnimation, transform.position, (Quaternion.Euler(0f, Random.Range(0.0f, 360.0f),0f)));
         Destroy(gameObject);
     }
