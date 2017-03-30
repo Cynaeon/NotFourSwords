3281daebf66efb3e8a5f91018ca111e01a4522a6
The13Destroyer <the13destroyer@cock.li>
2017-03-30 08:39:21 +0300

    WIP elevator scene. To be continued later today.

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pot : MonoBehaviour {
     
-    public GameObject heart;
+    public GameObject prefab;
     public GameObject breakAnimation;
 
     public void OnTriggerEnter(Collider other)
@@ -28,7 +28,7 @@ public class Pot : MonoBehaviour {
         Destroy(gameObject.GetComponent<CapsuleCollider>());
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
         Quaternion rot = Quaternion.Euler(-90, 0, 0);
-        Instantiate(heart, pos, rot);
+        Instantiate(prefab, pos, rot);
         Instantiate(breakAnimation, transform.position, (Quaternion.Euler(0f, Random.Range(0.0f, 360.0f),0f)));
         Destroy(gameObject);
     }
