dda83239be905378a410c61b769d2b40464307bd
Mutsis <bigjorma69@hotmale.com>
2017-04-27 18:21:04 +0300

    D E T A I L S

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -336,7 +336,7 @@ public class Elevator : MonoBehaviour {
         {
             if (other == playerControls[i].playerHitbox.GetComponent<Collider>())
             {
-                //canvases[i].GetComponent<UIManager>().EnableNotification(true);
+                gameManager.canvases[i].GetComponent<UIManager>().EnableNotification(true);
                 _isColliding[i] = true;
             }
         }
@@ -348,7 +348,7 @@ public class Elevator : MonoBehaviour {
         {
             if (other == playerControls[i].playerHitbox.GetComponent<Collider>())
             {
-                //canvases[i].GetComponent<UIManager>().EnableNotification(false);
+                gameManager.canvases[i].GetComponent<UIManager>().EnableNotification(false);
                 _isColliding[i] = false;
                 
             }
