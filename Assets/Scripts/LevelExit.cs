ddacc2af44b56c5f318c2c0fcec9d3941e554117
TAMK\c5jtasal <jonitasala@gmail.com>
2017-05-12 16:27:20 +0300

    Fixed scene spawning in the 4th floor. New snow particle sprite on death.

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -36,8 +36,8 @@ public class LevelExit : MonoBehaviour
         Corridor_Torch = 18,
         Floor3EastCorridor = 19,
         LensPuzzle = 20,
-        Floor4SouthCorridor = 21,
-        Floor4NorthCorridor = 22
+        Floor4NorthCorridor = 21,
+        Floor4SouthCorridor = 22
     }
 
     public Level level;
